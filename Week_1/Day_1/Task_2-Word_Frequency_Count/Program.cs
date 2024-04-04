57a4b33f827d5d69f182b620fe0cbe4ba64680a5
Farouk Sedick <sadikalhanssah@gmail.com>
2024-04-04 10:11:40 +0000

    Update Program.cs

--- Week_1/Day_1/Task_2-Word_Frequency_Count/Program.cs
+++ Week_1/Day_1/Task_2-Word_Frequency_Count/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 public class App{
     public static void Main(string[] args){
         /* TESTS */
-        List<string> Tests = ["Coding is interesting ?", "My name is Farouk", "I am learning C-Sharp", "Recursion is Recursion"];
+        List<string> Tests = ["?????", "f?a*r&k", "     ", "Coding is interesting ?", "My name is Farouk", "I am learning C-Sharp", "Recursion is Recursion"];
 
         foreach(string test in Tests){
             WordFreqCount(test).Select(item=>item.Key+": "+item.Value.ToString()).ToList().ForEach(Console.WriteLine);
