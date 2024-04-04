6967ef2ef1ea8b6673a00b3b09e9b226993d500d
Far9999 <sadikalhanssah@gmail.com>
2024-04-04 03:10:30 +0000

    finish task 3

---
+++ Week_1/Day_1/Task_3-Palindrome_Check/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class App
+{
+    public static void Main(string[] args){
+        List<string> Tests = ["abcba", "asb"];
+
+        foreach(string test in Tests){
+            Console.WriteLine(PalindromeCheck(test));
+        }
+    }
+
+    public static bool PalindromeCheck(string s){
+        s = s.Trim().ToLower().Replace(" ", "");
+        string new_s = new(s.Where(ch=>!Char.IsPunctuation(ch)).ToArray());
+        return new_s==new string(new_s.Reverse().ToArray());
+    }
+}
