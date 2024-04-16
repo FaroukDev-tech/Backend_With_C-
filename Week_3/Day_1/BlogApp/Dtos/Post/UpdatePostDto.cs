c68a56d44e7948f69f6202e2577b33d42cbbe738
Far9999 <sadikalhanssah@gmail.com>
2024-04-16 19:20:19 +0000

    remove old blogapp

--- Week_3/Day_1/BlogApp/Dtos/Post/UpdatePostDto.cs
+++
@@ -1,9 +0,0 @@
-namespace BlogApp.Dtos.Post
-{
-    public class UpdatePostDto
-    {
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public DateTime CreatedAt {get; set;}
-    }
-}
